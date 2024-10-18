d9dc21e475fd7391f39443b2651ee65f02d36b4f
yashprasad234 <yashprasad272@gmail.com>
2024-10-18 18:52:46 +0530

    fixed appbar

--- components/Appbar.vue
+++ components/Appbar.vue
@@ -79,18 +79,7 @@ onBeforeMount(() => {
             }
           "
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-6"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <IconHamburger />
         </div>
         <div
           v-if="hamburgerMenuOpen"
@@ -101,28 +90,18 @@ onBeforeMount(() => {
             }
           "
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-6"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <IconClose />
         </div>
       </div>
       <div v-if="hamburgerMenuOpen" class="flex flex-col items-start">
         <div
           v-for="([name, href], index) in menuState.menu"
           class="flex justify-between gap-16"
+          :key="index"
         >
-          <MenuButton :key="index" :name="name" :href="href" />
+          <MenuButton :name="name" :href="href" />
         </div>
-        <div v-if="userState.user.role == ''">
+        <div v-if="userState.user.role == ''" class="flex flex-col items-start">
           <MenuButton name="Login" href="/login" />
           <MenuButton name="Register" href="/signup" />
         </div>
@@ -136,20 +115,7 @@ onBeforeMount(() => {
               }
             "
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-9"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-              />
-            </svg>
+            <IconUser />
           </div>
           <!-- User menu -->
           <div
@@ -186,7 +152,7 @@ onBeforeMount(() => {
           >
             <div
               v-for="([name, href], index) in menuState.menu"
-              class="flex justify-between gap-16"
+              class="flex justify-between gap-4"
             >
               <MenuButton :key="index" :name="name" :href="href" />
             </div>
@@ -200,22 +166,23 @@ onBeforeMount(() => {
           />
           <div
             v-if="!userState?.isLoading && userState?.user.role == ''"
-            class="flex gap-16 items-center"
+            class="flex gap-6 items-center"
           >
             <div
               v-for="([name, href], index) in menuState.menu"
-              class="flex justify-between gap-16"
+              class="flex justify-between gap-4"
+              :key="index"
             >
-              <MenuButton :key="index" :name="name" :href="href" />
+              <MenuButton :name="name" :href="href" />
             </div>
             <button
               @click="navigateTo(`/login`)"
-              class="border border-deep-orange bg-white hover:bg-deep-orange px-4 py-2 hover:text-neutral-100 font-bold"
+              class="bg-gray-100 hover:bg-deep-orange px-4 py-2 hover:text-neutral-100 font-bold"
             >
               Login
             </button>
             <button
-              class="border border-deep-orange bg-deep-orange px-4 py-2 text-neutral-100 font-bold"
+              class="bg-deep-orange px-4 py-2 text-neutral-100 font-bold"
               @click="navigateTo(`/signup`)"
             >
               Register
